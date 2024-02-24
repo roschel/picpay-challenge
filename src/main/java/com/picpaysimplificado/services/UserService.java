3f8ba62494349a07ba8b54a5dd20ac2f492400bb
roschel <joaomroschel@gmail.com>
2024-02-24 01:30:48 -0300

    feat: update User service

--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -2,11 +2,14 @@ package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -29,5 +32,15 @@ public class UserService {
 
     public void saveUser(User user){
         this.repository.save(user);
+    }
+
+    public User createUser(UserDTO userDTO) {
+        User newUser = new User(userDTO);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
     }
 }
