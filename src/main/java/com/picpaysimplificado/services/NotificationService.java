52835613d76c84355244649cbd5baf9ba8eb122e
roschel <joaomroschel@gmail.com>
2024-02-24 01:09:12 -0300

    feat: create Notification service

---
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,27 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String messgae) throws Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, messgae);
+        ResponseEntity<String> notificationResponse = restTemplate
+                .postForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("Erro ao enviar a notificação");
+            throw new Exception("Serviço de notificação indisponível");
+        }
+    }
+}
