9329b45697a1480ff2c51fae5fb2d758456c45bb
roschel <joaomroschel@gmail.com>
2024-02-24 01:30:26 -0300

    feat: update Transaction NoArgsConstructor

--- src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -4,6 +4,7 @@ import com.picpaysimplificado.domain.user.User;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "transactions")
 @Getter
 @Setter
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Transaction {
 
